aac1640779e85c542da0fd54d9c1e3e60142954e
Lim Qi Qing <101784318+Qds11@users.noreply.github.com>
2022-12-04 17:52:51 +0800

    update setup.sql

--- setup.sql
+++ setup.sql
@@ -4,13 +4,12 @@ create database BudgetTracking;
 use BudgetTracking;
 
 CREATE TABLE IF NOT EXISTS spending (
-id INT NOT NULL,
 date DATE NOT NULL,
 spendAmt DECIMAL(19,2) NOT NULL,
-PRIMARY KEY(id)
+PRIMARY KEY(date)
 );
 
-INSERT INTO spending (id,date, spendAmt) VALUES
+INSERT INTO spending (date, spendAmt) VALUES
 ('2022-08-02', '905.50'),
 ('2022-09-06', '12.50'),
 ('2022-09-15', '495.50'),
@@ -20,5 +19,4 @@ INSERT INTO spending (id,date, spendAmt) VALUES
 ('2022-10-22', '21.50'),
 ('2022-11-11', '52.34'),
 ('2022-11-21', '45.50'),
-('2022-11-26', '15.50'),
-;
+('2022-11-26', '15.50');
